e5a8cc8b59630cd0bb9c24e70c59178bf0438052
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-18 17:59:51 +0100

    Implementacao da ScrollView.

--- lib/main.dart
+++ lib/main.dart
@@ -21,19 +21,20 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
           backgroundColor: Colors.green,
         ),
         body: Container(
+            child: SingleChildScrollView(
           child: Column(
             children: [
               Padding(
-                padding: EdgeInsets.only(top: 100, bottom: 20),
+                padding: EdgeInsets.only(top: 100, bottom: 30),
                 child: Image.asset("images/logo.png"),
               ),
               Container(
-                 padding: EdgeInsets.only(bottom: 20),
+                padding: EdgeInsets.only(bottom: 20),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Image.asset("images/menu_empresa.png"),
-                    Image.asset("images/menu_empresa.png"),
+                    Image.asset("images/menu_servico.png"),
                   ],
                 ),
               ),
@@ -41,13 +42,13 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Image.asset("images/menu_empresa.png"),
-                    Image.asset("images/menu_empresa.png"),
+                    Image.asset("images/menu_cliente.png"),
+                    Image.asset("images/menu_contato.png"),
                   ],
                 ),
               )
             ],
           ),
-        ));
+        )));
   }
 }
