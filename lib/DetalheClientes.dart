f4dad400edab3232f56b68c1db8797c1e88198f5
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-18 22:48:44 +0100

    termino da tela clientes.

--- lib/DetalheClientes.dart
+++ lib/DetalheClientes.dart
@@ -17,6 +17,7 @@ class _DetalheClienteState extends State<DetalheCliente> {
         child: Container(
           padding: EdgeInsets.all(16),
           child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
@@ -29,11 +30,14 @@ class _DetalheClienteState extends State<DetalheCliente> {
               ),
               Padding(
                 padding: EdgeInsets.only(top: 16),
-                child: Text(
-                    "Com o avanço da tecnologia e o mundo em constante desenvolvimento é notável o uso constante e diário das mídias socias (concretamente as TICs) para facilitar a comunicação entre os seres humanos. Ademais o uso das TICs nos é imposta pela real necessidade social, pois embora ela apresente diversas características. A partilha de informações no seio dos académicos é um meio pela qual se estabelece uma comunicação clara e persistente. /n Lembrando que hoje em dia as universidades usam as TICs para poder partilhar informações internas e externas das mesmas para os académicos e para a sociedade em geral, isto é, publicando arquivos científicos, criando debates e assuntos pertinentes e de caracter importantes para o desenvolvimento da sociedade. E olhando para realidade de outras universidades comparando a nossa no que tange o assunto em causa, notamos uma grande diferença no âmbito da difusão da informação via digital."
-                    "Com o avanço da tecnologia e o mundo em constante desenvolvimento é notável o uso constante e diário das mídias socias (concretamente as TICs) para facilitar a comunicação entre os seres humanos. Ademais o uso das TICs nos é imposta pela real necessidade social, pois embora ela apresente diversas características. A partilha de informações no seio dos académicos é um meio pela qual se estabelece uma comunicação clara e persistente. /n Lembrando que hoje em dia as universidades usam as TICs para poder partilhar informações internas e externas das mesmas para os académicos e para a sociedade em geral, isto é, publicando arquivos científicos, criando debates e assuntos pertinentes e de caracter importantes para o desenvolvimento da sociedade. E olhando para realidade de outras universidades comparando a nossa no que tange o assunto em causa, notamos uma grande diferença no âmbito da difusão da informação via digital."
-                    "Com o avanço da tecnologia e o mundo em constante desenvolvimento é notável o uso constante e diário das mídias socias (concretamente as TICs) para facilitar a comunicação entre os seres humanos. Ademais o uso das TICs nos é imposta pela real necessidade social, pois embora ela apresente diversas características. A partilha de informações no seio dos académicos é um meio pela qual se estabelece uma comunicação clara e persistente. /n Lembrando que hoje em dia as universidades usam as TICs para poder partilhar informações internas e externas das mesmas para os académicos e para a sociedade em geral, isto é, publicando arquivos científicos, criando debates e assuntos pertinentes e de caracter importantes para o desenvolvimento da sociedade. E olhando para realidade de outras universidades comparando a nossa no que tange o assunto em causa, notamos uma grande diferença no âmbito da difusão da informação via digital."),
-              )
+                child: Image.asset("images/cliente1.png"),
+              ),
+              Text("Empresa de Software"),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Image.asset("images/cliente2.png"),
+              ),
+              Text("Empresa de Auditoria"),
             ],
           ),
         ),
